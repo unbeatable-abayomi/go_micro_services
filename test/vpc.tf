efca30eb74c395137c3f29c4203dba624c4314ff
unbeatable-abayomi <igwubor@outlook.com>
2025-12-22 13:12:26 +0100

    Modified VPC Tf

--- test/vpc.tf
+++ test/vpc.tf
@@ -6,7 +6,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "abayomi-eks-${random_string.suffix.result}"
+  cluster_name = "kube-masters-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
